c1ea8839fa835bdc32c21d32c3dd5849155ec552
Illia23 <Priymak2331@gmail.com>
2025-05-08 21:43:19 +0200

    final

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './Components/App/App';
 import React from 'react';
 import ReactDOM from "react-dom";
 import "./i18n.js"
+import 'normalize.css';
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
